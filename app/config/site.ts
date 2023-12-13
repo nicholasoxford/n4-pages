075bbc064047d09a3e2effaf937630849d599563
Nicholas Oxford <maheledaily@gmail.com>
2023-12-12 22:40:23 -0700

    fix config

--- app/config/site.ts
+++ app/config/site.ts
@@ -2,6 +2,10 @@ export const siteConfig = {
   name: "PACKAGE_NAME_REPLACE",
   description:
     "Go From Zero to Full-Stack Application in under a minute with Cloudflare D1, R2, and Pages + Supabase Auth",
+  links: {
+    twitter: "https://twitter.com/ApolloToday",
+    github: "https://github.com/nicholasoxford",
+  },
 };
 
 export type SiteConfig = typeof siteConfig;
